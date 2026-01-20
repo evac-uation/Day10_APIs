a8d2ab7742293b09e4dbb97756fad6ef875e3daf
evchen-eps <evchen@eastsideprep.org>
2026-01-19 23:40:44 -0800

    deletefunction and challenge 1

--- server.js
+++ server.js
@@ -1,25 +1,45 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
-const PORT = process.env.Port || 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
 app.use(express.static('public'));
 
-let dataStore = [{ name: "Teacher", message: "API is live!"}];
+let dataStore = [
+    { name: "Teacher", message: "API is live!" }
+];
+
+app.post('/api/messages', (req, res) => {
+    const newMessage = {
+        name: req.body.name,
+        message: req.body.message
+    };
+
+    if (newMessage.name.length == 0 || newMessage.message.length < 5) {
+        res.status(400).json('');
+        return;
+    }
+
+    dataStore.push(newMessage);
+    res.status(201).json(newMessage);
+});
+
 
 //GET: Send data from the datastore to the user
 app.get('/api/messages', (req, res) => {
     res.json(dataStore);
 });
 
-//POST: receive data from the form
-app.post('/api/messages', (req, res) => {
-    dataStore.push(req.body);
-    res.status(201).send({message:"Received!"});
+// DELETE: Remove a message by its index
+app.delete('/api/messages/:id', (req, res) => {
+    dataStore.splice(req.params.id, 1);
+    res.json({ message: "Deleted!" });
 });
 
-app.listen(PORT, () => console.log ('Server: http://localhost:3000'));
+app.listen(PORT, () => console.log (`Server: http://localhost:${PORT}`));
+
+
 
 //Reminder: type node server.js in terminal to start it running
